7cf02d6e5121888d6fe9f5a050327b4a024b1301
daehee.park <pdh7360@gmail.com>
2023-05-24 22:01:24 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+## reference
+
+### json
+https://hianna.tistory.com/629
+https://dejavuhyo.github.io/posts/generating-parsing-json-using-gson/
--- src/sp_java/JsonSample.java
+++ src/sp_java/JsonSample.java
@@ -26,8 +26,4 @@ public class JsonSample {
 		
 	}
 	
-
-	//https://hianna.tistory.com/629
-	//https://dejavuhyo.github.io/posts/generating-parsing-json-using-gson/
-
 }
