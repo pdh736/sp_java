29493b7489a95955bf5907848945f1e13d49491a
daehee.park <pdh7360@gmail.com>
2023-06-11 15:48:33 +0900

    Add scanner to main

--- src/sp_java/Main.java
+++ src/sp_java/Main.java
@@ -1,5 +1,7 @@
 package sp_java;
 
+import java.util.Scanner;
+
 import sp_java.json.JsonWrapper;
 import sp_java.json.Student;
 
@@ -7,7 +9,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		JsonWrapper<Student> jw = new JsonWrapper<Student>() {};
-
+		
+		Scanner sc = new Scanner(System.in);
+		String path = sc.next();
+	
 	}
-
 }
